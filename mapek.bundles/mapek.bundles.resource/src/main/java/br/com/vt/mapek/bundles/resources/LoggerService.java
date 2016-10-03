e607ede6341bfec239e9b14e66269b15d94af003
Vinicius <contato@viniciustrindade.com.br>
2016-10-03 12:17:17 -0300

    Common File created, Sorts created

---
+++ mapek.bundles/mapek.bundles.resource/src/main/java/br/com/vt/mapek/bundles/resources/LoggerService.java
@@ -0,0 +1,59 @@
+package br.com.vt.mapek.bundles.resources;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
+
+import br.com.vt.mapek.services.ILoggerService;
+
+class LoggerService implements ILoggerService, LogService {
+
+
+	public void log(int level, String message) {
+		console(level, message);
+
+	}
+
+	public void log(int level, String message, Throwable exception) {
+		console(level, message);
+
+	}
+
+	public void log(ServiceReference sr, int level, String message) {
+		console(level, message);
+
+	}
+
+	public void log(ServiceReference sr, int level, String message,
+			Throwable exception) {
+		console(level, message);
+
+	}
+	
+	public void I(String message) {
+		log(LOG_INFO, message);
+	}
+
+	public void E(String message) {
+		log(LOG_ERROR, message);
+	}
+
+	public void W(String message) {
+		log(LOG_WARNING, message);
+	}
+
+	public void D(String message) {
+		log(LOG_DEBUG, message);
+
+	}
+	public void console(int level, String message) {
+		switch (level) {
+		case LOG_ERROR:
+			System.err.println(message);
+			break;
+		default:
+			System.out.println(message);
+			break;
+		}
+
+	}
+}
---
+++ mapek.bundles/mapek.bundles.resource/src/main/java/br/com/vt/mapek/bundles/resources/RandomGenerator.java
@@ -0,0 +1,52 @@
+package br.com.vt.mapek.bundles.resources;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class RandomGenerator {
+	public static final String FILENAME = "src/main/resources/" + "input.csv";
+	public static final int FILESIZE_77656_KB = 10000000; // 77,6 MB
+	public static final int FILESIZE_6788_KB = 1000000; // 6,7 MB
+	public static final int FILESIZE_584_KB = 100000; // 584 KB
+	public static final int FILESIZE_56_KB = 10000; // 56 KB
+	
+	public static int SIZE = FILESIZE_56_KB;
+	
+	
+	public static void main(String[] args) throws IOException {
+		FileService fileService = new FileService(new LoggerService());
+		
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=1; i<SIZE;i++) {
+            list.add(i);
+        }
+        
+        Collections.shuffle(list);
+        
+        
+        OutputStream out = fileService.getOutputStream(FILENAME);
+        PrintWriter writer = new PrintWriter(out);
+        StringBuffer buffer= new StringBuffer();
+        for (Iterator<Integer>it = list.iterator();it.hasNext();){
+        	
+        	buffer.append(it.next());
+        	if (it.hasNext()){
+        		buffer.append(",");
+        	}
+        	
+        }
+        writer.print(buffer.toString());
+        writer.close();
+    }
+	
+	
+	
+}
